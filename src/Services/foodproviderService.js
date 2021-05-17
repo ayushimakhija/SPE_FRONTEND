5818f60e72d86f87aa32596fa3ba417837d608a4
pavansinghrajput <62704075+pavansinghrajput@users.noreply.github.com>
2021-05-17 21:24:37 +0530

    Update foodproviderService.js

--- src/Services/foodproviderService.js
+++ src/Services/foodproviderService.js
@@ -2,19 +2,19 @@ import axios from "axios";
 class foodproviderService {
 
     getAllFoodprovider() {
-        return axios.get("http://localhost:8080/api/getAllFoodprovider");
+        return axios.get("http://localhost:8082/api/getAllFoodprovider");
     }
 
     getAllFoodproviderByLocation(location) {
-        return axios.get("http://localhost:8080/api/foodprovider/{location}")
+        return axios.get("http://localhost:8082/api/foodprovider/{location}")
     }
 
     createFoodprovider(foodprovider) {
-        return axios.post("http://localhost:8080/api/createfoodprovider", foodprovider);
+        return axios.post("http://localhost:8082/api/createfoodprovider", foodprovider);
     }
 
     updateFoodprovider(foodprovider) {
-        return axios.put("http://localhost:8080/api/updatefoodprovider/{id}", foodprovider);
+        return axios.put("http://localhost:8082/api/updatefoodprovider/{id}", foodprovider);
     }
 }
 export default foodproviderService
