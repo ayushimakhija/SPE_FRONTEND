a5cac9f657990630a4e3115f292fb5941b8686d7
pavansinghrajput <62704075+pavansinghrajput@users.noreply.github.com>
2021-05-17 21:25:19 +0530

    Update customerService.js

--- src/Services/customerService.js
+++ src/Services/customerService.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 class customerService {
     getAllCustomers() {
-        return axios.get("http://localhost:8080/api/getAllCustomers");
+        return axios.get("http://localhost:8082/api/getAllCustomers");
     }
     getCustomerById(){
-        return axios.get("http://localhost:8080/api/getCustomerById")
+        return axios.get("http://localhost:8082/api/getCustomerById")
     }
     createCustomer(customer)
     {
-        return axios.post("http://localhost:8080/api/createCustomer",customer);
+        return axios.post("http://localhost:8082/api/createCustomer",customer);
     }
     updateCustomer(customer){
-        return axios.put("http://localhost:8080/api/updateCustomer",customer);
+        return axios.put("http://localhost:8082/api/updateCustomer",customer);
     }
 
 }
