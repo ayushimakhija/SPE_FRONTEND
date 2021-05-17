c4573554423ea86c094344cbcaa1fcef46b38990
pavansinghrajput <62704075+pavansinghrajput@users.noreply.github.com>
2021-05-17 21:25:45 +0530

    Update menuService.js

--- src/Services/menuService.js
+++ src/Services/menuService.js
@@ -3,13 +3,13 @@ import axios from "axios";
 class menuService{
 
     getMenuByDish(dishName){
-        return axios.get("http://localhost:8080/api//menu/{dishName}");
+        return axios.get("http://localhost:8082/api//menu/{dishName}");
     }
     createMenu(menu)
     {
-        return axios.post("http://localhost:8080/api/createMenu",menu);
+        return axios.post("http://localhost:8082/api/createMenu",menu);
     }
     updateMenu(menu){
-        return axios.put("http://localhost:8080/api/updateMenu/{id}",menu);
+        return axios.put("http://localhost:8082/api/updateMenu/{id}",menu);
     }
 }
